5bdeec792f46381dbbd1b3bbcd240b95a881b28d
Mario Romero <mario.romero@cftsanagustin.cl>
2023-11-22 19:14:24 -0300

    Agregar archivos de proyecto.

---
+++ AjaxEjemplo/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using AjaxEjemplo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AjaxEjemplo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public JsonResult testAjax()
+        {
+            string mensaje = "hola desde método";
+
+            bool usuarioLogeado = false;
+
+
+            /*if (usuarioLogeado)
+            {
+                return View("")
+            }
+            else
+            {
+                return Json(mensaje);
+            }*/
+
+            return Json(mensaje);
+        }
+    }
+}
